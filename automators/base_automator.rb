08aaad91e5684b49779fce7598c0bcec0f230dfd
Oliver Spindler <os@oliverspindler.com>
2013-08-13 18:26:49 +0100

    Add mouse click to automator

--- automators/base_automator.rb
+++ automators/base_automator.rb
@@ -6,4 +6,6 @@ class BaseAutomator
   def mouse_move (x, y)
   end
 
+  def mouse_click (button=1)
+  end
 end
--- automators/osxautomation_automator.rb
+++ automators/osxautomation_automator.rb
@@ -7,4 +7,8 @@ class OsxautomationAutomator < BaseAutomator
   def mouse_move (x, y)
     system( "osxautomation \"mousemove #{x.round} #{y.round}\" > /dev/null" )
   end
+
+  def mouse_click (button=1)
+    system( "osxautomation \"mouseclick #{button}\" > /dev/null" )
+  end
 end
