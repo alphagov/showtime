27a0543f07029f251011f5bf4ff42541d5313271
Ben Marvell <madmarv187@gmail.com>
2014-07-31 15:05:27 +0100

    Carers allowance has much more module viewing.
    We also visit user-satisfaction
    We sort tables.

--- journeys/carers_allowance.rb
+++ journeys/carers_allowance.rb
@@ -5,14 +5,41 @@ class CarersAllowanceJourney < BaseJourney
     sleep 5
 
     sections = ['transactions-per-year',
-                'volumetrics',
-                'user-satisfaction',
-                'service-availability']
+                  'volumetrics',
+                'user-satisfaction']
 
     sections.each do |id|
       scrollToEl(@browser.element(:id => id))
       sleep 4
     end
+
+    moveToEl(@browser.link(:text => "User satisfaction"), { :click => true })
+    sleep 5
+
+    d = @browser.element(:class => "y-axis")
+    moveToEl(d, { :vertical => 0.5, :horizontal => 3.0 })
+    sleep 2
+    moveToEl(d, { :vertical => 0.5, :horizontal =>  6.0})
+    sleep 2
+    moveToEl(d, { :vertical => 0.5, :horizontal =>  9.0})
+    sleep 2
+    moveToEl(d, { :vertical => 0.5, :horizontal =>  12.0})
+    sleep 4
+
+    scrollToEl(@browser.element(:class => "floated-header"))
+    sleep 4
+
+    moveToEl(@browser.link(:text => "Very satisfied"), { :click => true })
+    sleep 4
+
+    moveToEl(@browser.link(:text => "Very satisfied"), { :click => true })
+    sleep 4
+
+    moveToEl(@browser.link(:text => "Carer's Allowance applications"), { :click => true })
+    sleep 5
+
+    scrollToEl(@browser.element(:id => "users-at-each-step"))
+    sleep 4
   end
 
 end
